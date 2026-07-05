ccae0eb68e9d0b425be9d6df99d3021a44aa8a09
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-07-04 22:21:34 -0300

    refactor: replace repository interfaces with abstract classes

--- src/domain/forum/application/repositories/answer-comment-repository.ts
+++ src/domain/forum/application/repositories/answer-comment-repository.ts
@@ -1,9 +1,9 @@
 import { PaginationParams } from "@/src/core/repositories/pagination-params";
 import type { AnswerComment } from "../../enterprise/entities/answer-comment";
 
-export interface AnswerCommentRepository {
-    findById(id: string): Promise<AnswerComment | null>
-    findManyByAnswerId(answerId: string, params: PaginationParams): Promise<AnswerComment[]>
-    create(answerComment: AnswerComment): Promise<void>
-    delete(answerComment: AnswerComment): Promise<void>
+export abstract class AnswerCommentRepository {
+    abstract findById(id: string): Promise<AnswerComment | null>
+    abstract findManyByAnswerId(answerId: string, params: PaginationParams): Promise<AnswerComment[]>
+    abstract create(answerComment: AnswerComment): Promise<void>
+    abstract delete(answerComment: AnswerComment): Promise<void>
 }
--- src/domain/forum/application/repositories/question-comment-repository.ts
+++ src/domain/forum/application/repositories/question-comment-repository.ts
@@ -1,9 +1,9 @@
 import { PaginationParams } from "@/src/core/repositories/pagination-params"
 import type { QuestionComment } from "../../enterprise/entities/question-comment";
 
-export interface QuestionCommentRepository {
-    findById(id: string): Promise<QuestionComment | null>
-    findManyByQuestionId(questionId: string, params: PaginationParams): Promise<QuestionComment[]>
-    create(questionComment: QuestionComment): Promise<void>
-    delete(questionComment: QuestionComment): Promise<void>
+export abstract class QuestionCommentRepository {
+    abstract findById(id: string): Promise<QuestionComment | null>
+    abstract findManyByQuestionId(questionId: string, params: PaginationParams): Promise<QuestionComment[]>
+    abstract create(questionComment: QuestionComment): Promise<void>
+    abstract delete(questionComment: QuestionComment): Promise<void>
 }
